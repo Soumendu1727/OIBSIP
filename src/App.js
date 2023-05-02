811cd61cc3a94627a57410518bb1f26c84acd898
Soumendu Sadhukhan <81556109+Soumendu1727@users.noreply.github.com>
2023-05-03 01:42:00 +0530

    Landing Page

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Hero from './components/hero/Hero'
+import Navbar from './components/navbar/Navbar'
+import Data from './components/data/Data'
+import Cloud from './components/cloud/Cloud'
+import Footer from './components/footer/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Data  />
+      <Cloud />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
